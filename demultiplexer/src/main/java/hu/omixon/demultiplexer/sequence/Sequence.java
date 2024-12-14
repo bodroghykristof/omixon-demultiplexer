46d0709e0da781a5f1ecaec855f579ffb5d6eb9f
bodroghykristof <bodroghykristof@gmail.com>
2024-12-14 16:33:18 +0100

    disallow creating sequence without base chain

--- demultiplexer/src/main/java/hu/omixon/demultiplexer/sequence/Sequence.java
+++ demultiplexer/src/main/java/hu/omixon/demultiplexer/sequence/Sequence.java
@@ -2,4 +2,12 @@ package hu.omixon.demultiplexer.sequence;
 
 import java.util.List;
 
-public record Sequence(List<NucleotideBase> nucleotideBaseChain) {}
+public record Sequence(List<NucleotideBase> nucleotideBaseChain) {
+
+    public Sequence {
+        if (nucleotideBaseChain == null || nucleotideBaseChain.isEmpty()) {
+            throw new IllegalArgumentException("nucleotideBaseChain cannot be null or empty");
+        }
+    }
+
+}
