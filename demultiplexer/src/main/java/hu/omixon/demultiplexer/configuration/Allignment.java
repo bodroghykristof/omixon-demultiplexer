08951cf1792ebd3a05447df929cc73d76f7c0ec1
bodroghykristof <bodroghykristof@gmail.com>
2024-12-14 15:52:33 +0100

    add name property for Allignment

--- demultiplexer/src/main/java/hu/omixon/demultiplexer/configuration/Allignment.java
+++ demultiplexer/src/main/java/hu/omixon/demultiplexer/configuration/Allignment.java
@@ -1,7 +1,18 @@
 package hu.omixon.demultiplexer.configuration;
 
+import lombok.Getter;
+
+@Getter
 public enum Allignment {
 
-    ENDS, MID, BEST
+    ENDS("endsAlignment"),
+    MID("midAlignment"),
+    BEST("bestAlignment");
+
+    private final String name;
+
+    Allignment(String name) {
+        this.name = name;
+    }
 
 }
