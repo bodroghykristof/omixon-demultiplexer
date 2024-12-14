c964cb9741763253f7e9ad057e33ba458f3f9541
bodroghykristof <bodroghykristof@gmail.com>
2024-12-14 17:17:39 +0100

    add logging library

--- demultiplexer/src/main/java/hu/omixon/demultiplexer/service/DemultiplexerService.java
+++ demultiplexer/src/main/java/hu/omixon/demultiplexer/service/DemultiplexerService.java
@@ -6,8 +6,9 @@ import hu.omixon.demultiplexer.configuration.DemultiplexerConfiguration;
 import hu.omixon.demultiplexer.configuration.result.DemultiplexerResult;
 import hu.omixon.demultiplexer.sequence.SequenceSample;
 import lombok.AllArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
 
-@AllArgsConstructor
+@AllArgsConstructor @Slf4j
 public class DemultiplexerService {
 
     private final IOService ioService;
@@ -15,11 +16,15 @@ public class DemultiplexerService {
     public void run(String sequenceSampleFilePath, String configFilePath,
                     Allignment allignment, String outputFilePrefix) {
 
-        SequenceSample sequenceSample = ioService.readSequences(sequenceSampleFilePath);
-        DemultiplexerConfiguration configuration = ioService.readConfiguration(configFilePath);
-        ConfigSection configSection = configuration.findSectionByAllignment(allignment);
-        DemultiplexerResult result = configSection.splitSequenceToGroups(sequenceSample);
-        ioService.writeResultToFile(result, outputFilePrefix);
+        try {
+            SequenceSample sequenceSample = ioService.readSequences(sequenceSampleFilePath);
+            DemultiplexerConfiguration configuration = ioService.readConfiguration(configFilePath);
+            ConfigSection configSection = configuration.findSectionByAllignment(allignment);
+            DemultiplexerResult result = configSection.splitSequenceToGroups(sequenceSample);
+            ioService.writeResultToFile(result, outputFilePrefix);
+        } catch (Exception e) {
+            log.error("An error occurred while performing operation.", e);
+        }
 
     }
 
