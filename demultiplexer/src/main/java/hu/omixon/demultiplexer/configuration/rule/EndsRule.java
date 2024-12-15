4421816d51cf733437175438c445f1f766af5159
bodroghykristof <bodroghykristof@gmail.com>
2024-12-15 12:06:18 +0100

    add logic to EndsRule

--- demultiplexer/src/main/java/hu/omixon/demultiplexer/configuration/rule/EndsRule.java
+++ demultiplexer/src/main/java/hu/omixon/demultiplexer/configuration/rule/EndsRule.java
@@ -4,10 +4,24 @@ import hu.omixon.demultiplexer.sequence.Sequence;
 
 public record EndsRule(Sequence prefix, Sequence postfix) implements ConfigRule {
 
+    public EndsRule {
+        if (prefix == null || postfix == null) {
+            throw new IllegalArgumentException("Both prefix and postfix must be defined");
+        }
+    }
+
     @Override
     public int getMatchValue(Sequence sequence) {
-        // TODO
-        return 1;
+
+        if (sequence == null) {
+            throw new IllegalArgumentException("Cannot run rule on null sequence");
+        }
+
+        String prefixAsString = this.prefix.toBaseChainString();
+        String postfixAsString = this.postfix.toBaseChainString();
+        String sequenceAsString = sequence.toBaseChainString();
+
+        return sequenceAsString.startsWith(prefixAsString) && sequenceAsString.endsWith(postfixAsString) ? 1 : 0;
     }
 
 }
