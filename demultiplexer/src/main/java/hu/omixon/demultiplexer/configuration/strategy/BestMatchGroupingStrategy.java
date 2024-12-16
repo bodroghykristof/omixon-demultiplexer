a817a8b2c413a07acaf9aa5d43d9002fee08d67d
bodroghykristof <bodroghykristof@gmail.com>
2024-12-16 10:54:57 +0100

    add skeleton and validation tests for BestMatchGroupingStrategy

--- demultiplexer/src/main/java/hu/omixon/demultiplexer/configuration/strategy/BestMatchGroupingStrategy.java
+++ demultiplexer/src/main/java/hu/omixon/demultiplexer/configuration/strategy/BestMatchGroupingStrategy.java
@@ -2,6 +2,7 @@ package hu.omixon.demultiplexer.configuration.strategy;
 
 import hu.omixon.demultiplexer.configuration.ConfigGroupDefinition;
 import hu.omixon.demultiplexer.configuration.result.DemultiplexerResult;
+import hu.omixon.demultiplexer.sequence.Sequence;
 import hu.omixon.demultiplexer.sequence.SequenceSample;
 
 import java.util.List;
@@ -10,8 +11,18 @@ public class BestMatchGroupingStrategy implements GroupingStrategy {
 
     @Override
     public DemultiplexerResult splitSequenceToGroups(SequenceSample sample, List<ConfigGroupDefinition> groupDefinitions) {
-        // TODO
-        return new DemultiplexerResult();
+
+        if (sample == null || groupDefinitions == null) {
+            throw new IllegalArgumentException("Both sample and groupDefinitions must be provided");
+        }
+
+        DemultiplexerResult result = new DemultiplexerResult();
+
+        // TODO add logic
+
+        result.collectUnmatchedSequences(sample.sequences());
+        return result;
+
     }
 
 }
