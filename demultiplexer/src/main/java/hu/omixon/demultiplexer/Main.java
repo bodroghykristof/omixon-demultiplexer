add0bd5f983eb9288f9a7970431d7f5c60e0e7fc
bodroghykristof <bodroghykristof@gmail.com>
2024-12-16 15:34:06 +0100

    add log statements

--- demultiplexer/src/main/java/hu/omixon/demultiplexer/Main.java
+++ demultiplexer/src/main/java/hu/omixon/demultiplexer/Main.java
@@ -4,11 +4,16 @@ import hu.omixon.demultiplexer.commandline.CommandLineParams;
 import hu.omixon.demultiplexer.service.DataConversionService;
 import hu.omixon.demultiplexer.service.DemultiplexerService;
 import hu.omixon.demultiplexer.service.IOService;
+import lombok.extern.slf4j.Slf4j;
 
+import java.io.IOException;
+
+@Slf4j
 public class Main {
 
-    public static void main(String[] args) {
+    public static void main(String[] args) throws IOException {
 
+        log.info("Welcome to Omixon Demultiplexer. Starting process...");
         CommandLineParams params = CommandLineParams.fromArgs(args);
 
         IOService ioService = new IOService();
@@ -21,6 +26,8 @@ public class Main {
                 params.outputFilePrefix(),
                 params.allignmentName()
         );
+
+        log.info("Finished process.");
 
     }
 
